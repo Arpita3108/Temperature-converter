01e4e1cb9b397b0a9f3852ac7b813fe1f6955614
ArpitaMB <arpitambiradar3108@gmail.com>
2026-01-19 01:51:04 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,7 +5,6 @@ def celsius_to_fahrenheit(celsius: float) -> float:
 
 if __name__ == "__main__":
     default_celsius = 0.0
-    celsius = float(sys.argv[1]) if len(sys.argv) > 1 else default_celsius
-
+    celsius = 35
     fahrenheit = celsius_to_fahrenheit(celsius)
     print(f"Temperature in Fahrenheit: {fahrenheit}")
